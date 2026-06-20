8c8daec3e6e5c1bd48fac06bae0d0ce954dee055
joaquincernik <joaquincernik@gmail.com>
2026-06-20 13:30:02 -0300

    ajuste a twothirdes

--- ads.cpp
+++ ads.cpp
@@ -4,7 +4,7 @@
 #include <Wire.h>
 #include <Adafruit_ADS1X15.h>
 
-#define FACTOR_VOLTAJE   680.0f
+#define SENSITIVITY     680.0f
 #define FACTOR_CORRIENTE 11.1f
 
 static Adafruit_ADS1115 ads;
@@ -16,10 +16,29 @@ void initAds() {
         logPrintln("ERROR: ADS1115 no detectado en 0x48");
     } else {
         logPrintln("ADS1115 detectado en 0x48");
-        ads.setGain(GAIN_ONE);
+        ads.setGain(GAIN_TWOTHIRDS);
         ads.setDataRate(RATE_ADS1115_860SPS);
         logPrintln("ADS1115: PGA ±4.096V, 860SPS");
     }
+}
+
+static float leerTensionRMS() {
+    float Vsum = 0.0f;
+    for (int i = 0; i < 17; i++) {
+        Vsum += ads.computeVolts(ads.readADC_SingleEnded(0));
+        vTaskDelay(pdMS_TO_TICKS(1));
+    }
+    float zeroPoint = Vsum / 17.0f;
+
+    double sum_sq = 0.0;
+    for (int i = 0; i < 17; i++) {
+        float v = ads.computeVolts(ads.readADC_SingleEnded(0)) - zeroPoint;
+        sum_sq += (double)(v * v);
+        vTaskDelay(pdMS_TO_TICKS(1));
+    }
+
+    float rms_pin = sqrtf((float)(sum_sq / 17.0));
+    return rms_pin * SENSITIVITY;
 }
 
 static float calcular_RMS(uint8_t canal) {
@@ -46,10 +65,9 @@ void adsTask(void *pvParams) {
     static float corriente_filt = 0.0f;
 
     while (1) {
-        float rms_sensor_V = calcular_RMS(0);
-        float rms_sensor_A = calcular_RMS(1);
+        float tension_instantanea = leerTensionRMS();
 
-        float tension_instantanea = rms_sensor_V * FACTOR_VOLTAJE;
+        float rms_sensor_A = calcular_RMS(1);
         float corriente_instantanea = rms_sensor_A * FACTOR_CORRIENTE;
 
         voltaje_filt = 0.8f * voltaje_filt + 0.2f * tension_instantanea;
@@ -62,6 +80,6 @@ void adsTask(void *pvParams) {
         xQueueSend(adsDataQueue, &data, 0);
         logPrintln("Tensión: " + String(data.voltaje, 1) + "V, Corriente: " + String(data.corriente, 3) + "A");
 
-        vTaskDelay(pdMS_TO_TICKS(600));
+        vTaskDelay(pdMS_TO_TICKS(762));
     }
 }
